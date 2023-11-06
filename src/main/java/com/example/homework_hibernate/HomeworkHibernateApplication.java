31eb8ed34f07ba23544bbdc87b33caa87d6d4a90
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-06 18:15:31 +0300

    Initial Commit

---
+++ src/main/java/com/example/homework_hibernate/HomeworkHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_hibernate/HomeworkHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
