5b6f313ec205731dd9385d0fcc6154446a4ae522
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-06 18:52:55 +0300

    Added Controller

---
+++ src/main/java/com/example/homework_hibernate/controller/DbController.java
@@ -0,0 +1,24 @@
+package com.example.homework_hibernate.controller;
+
+import com.example.homework_hibernate.entity.Person;
+import com.example.homework_hibernate.repository.PersonsRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DbController {
+    private final PersonsRepository personsRepository;
+
+    public DbController(PersonsRepository personsRepository) {
+        this.personsRepository = personsRepository;
+    }
+
+    @GetMapping("/persons/by-city")
+    public List<Person> getPersonsByCity(@RequestParam String city) {
+        return personsRepository.getPersonsByCity(city);
+    }
+
+}
