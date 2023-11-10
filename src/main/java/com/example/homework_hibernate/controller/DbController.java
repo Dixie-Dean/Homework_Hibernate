7de06dc67b297faff8018d8a6349a10995485958
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-10 13:00:12 +0300

    Added required functionality to the Repository and Controller

--- src/main/java/com/example/homework_hibernate/controller/DbController.java
+++ src/main/java/com/example/homework_hibernate/controller/DbController.java
@@ -22,14 +22,28 @@ public class DbController {
         return personsRepository.findByCityOfLiving(city);
     }
 
+    @GetMapping("/persons-jpql/by-city")
+    public List<Person> getPersonsByCityJPQL(@RequestParam String city) {
+        return personsRepository.findByCity(city);
+    }
 
     @GetMapping("/persons/by-age")
     public List<Person> getPersonsByCity(@RequestParam Integer age) {
         return personsRepository.findPersonByPersonalData_Age(age);
     }
 
+    @GetMapping("/persons-jpql/by-age")
+    public List<Person> getPersonsByCityJPQL(@RequestParam Integer age) {
+        return personsRepository.findPersonByAge(age);
+    }
+
     @GetMapping("/persons/by-name-surname")
     public Optional<Person> getPersonsByNameAndSurname(@RequestParam String name, String surname) {
         return personsRepository.findPersonByPersonalData_NameAndPersonalData_Surname(name, surname);
+    }
+
+    @GetMapping("/persons-jpql/by-name-surname")
+    public Optional<Person> getPersonsByNameAndSurnameJPQL(@RequestParam String name, String surname) {
+        return personsRepository.findPersonByNameAndSurname(name, surname);
     }
 }
--- src/main/java/com/example/homework_hibernate/repository/PersonsRepository.java
+++ src/main/java/com/example/homework_hibernate/repository/PersonsRepository.java
@@ -2,6 +2,7 @@ package com.example.homework_hibernate.repository;
 
 import com.example.homework_hibernate.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,17 @@ import java.util.Optional;
 public interface PersonsRepository extends JpaRepository<Person, Long> {
     List<Person> findByCityOfLiving(String cityOfLiving);
 
+    @Query("select p from Person p where p.cityOfLiving = :cityOfLiving")
+    List<Person> findByCity(String cityOfLiving);
+
     List<Person> findPersonByPersonalData_Age(int personalData_age);
+
+    @Query("select p from Person p where p.personalData.age = :age")
+    List<Person> findPersonByAge(int age);
 
     Optional<Person> findPersonByPersonalData_NameAndPersonalData_Surname(
             String personalData_name, String personalData_surname);
+
+    @Query("select p from Person p where p.personalData.name = :name and p.personalData.surname = :surname")
+    Optional<Person> findPersonByNameAndSurname(String name, String surname);
 }
